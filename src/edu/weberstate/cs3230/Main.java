7e6ad24fe1d14c804c77f46ec1e459b7c852f3db
Parker Smith <parker@parkersmith.us>
2017-03-04 10:13:38 -0700

    Write miterm test program

--- src/edu/weberstate/cs3230/Main.java
+++ src/edu/weberstate/cs3230/Main.java
@@ -5,4 +5,25 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    public static int countWaysToJump(int n) {
+
+        if (n == 1 || n == 0)
+        {
+            return n;
+        }
+
+        int a = 1;
+        int b = 1;
+        int temp;
+
+        for (int x = n - 1; x > 0; x--)
+        {
+            temp = b;
+            b = b + a;
+            a = temp;
+        }
+
+        return b;
+    }
 }
---
+++ tests/edu/weberstate/cs3230/MainTest.java
@@ -0,0 +1,41 @@
+package edu.weberstate.cs3230;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by parker on 3/4/17.
+ */
+class MainTest {
+    @Test
+    void zero_returns_zero()
+    {
+        Assertions.assertEquals(0, Main.countWaysToJump(0));
+    }
+
+    @Test
+    void one_returns_one()
+    {
+        Assertions.assertEquals(1, Main.countWaysToJump(1));
+    }
+
+    @Test
+    void four_returns_five()
+    {
+        Assertions.assertEquals(5, Main.countWaysToJump(4));
+    }
+
+    @Test
+    void five_returns_eight()
+    {
+        Assertions.assertEquals(8, Main.countWaysToJump(5));
+    }
+
+    @Test
+    void _20_is_correct()
+    {
+        Assertions.assertEquals(10946, Main.countWaysToJump(20));
+    }
+}
