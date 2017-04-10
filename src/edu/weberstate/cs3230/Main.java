691e13a87cec58b7dac1503587dad841562b959b
Parker Smith <parker@parkersmith.us>
2017-04-09 20:48:41 -0600

    Test lambda refactoring

--- src/edu/weberstate/cs3230/Main.java
+++ src/edu/weberstate/cs3230/Main.java
@@ -1,8 +1,21 @@
 package edu.weberstate.cs3230;
 
+import javafx.event.EventHandler;
+import javafx.scene.control.ButtonBase;
+
+import java.awt.event.ActionEvent;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ButtonBase button = new ButtonBase() {
+            @Override
+            public void fire() {
+
+            }
+        };
+        button.setOnAction(event -> {
+
+        });
     }
 }
